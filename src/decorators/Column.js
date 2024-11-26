35b679360b0c1258b3f5a24df8d558ee1082c8fe
Nitin Bansal <nitinbansal1989@yahoo.co.in>
2024-11-27 02:06:47 +0530

    eval fix

--- src/decorators/Column.js
+++ src/decorators/Column.js
@@ -1,16 +1,15 @@
 import { Expose } from 'class-transformer';
 import { COLUMN_KEY, TABLE_COLUMN_KEYS } from './Constants.js';
 function Column(name) {
-    return function (target, property) {
-        const val = name ?? property;
-        let columnVals = Reflect.getMetadata(TABLE_COLUMN_KEYS, target);
-        if (!columnVals)
-            columnVals = [];
-        columnVals.push(property);
-        Reflect.defineMetadata(TABLE_COLUMN_KEYS, columnVals, target);
-        Reflect.defineMetadata(COLUMN_KEY, val, target, property);
-        return Expose()(target, property);
-    };
+  return function (target, property) {
+    const val = name ?? property;
+    let columnVals = Reflect.getMetadata(TABLE_COLUMN_KEYS, target);
+    if (!columnVals) columnVals = [];
+    columnVals.push(property);
+    Reflect.defineMetadata(TABLE_COLUMN_KEYS, columnVals, target);
+    Reflect.defineMetadata(COLUMN_KEY, val, target, property);
+    return Expose()(target, property);
+  };
 }
 export default Column;
 //# sourceMappingURL=Column.js.map
