de1b40247c552ba500b956cf42508f59b53ab0f5
Nitin Bansal <nitinbansal1989@yahoo.co.in>
2025-04-12 15:07:54 +0530

    refactor fix

--- index.js
+++
@@ -1,8 +0,0 @@
-import * as core from 'dblink-core';
-import 'reflect-metadata';
-import Context from './src/Context.js';
-import * as collection from './src/collection/index.js';
-import * as decorators from './src/decorators/index.js';
-import * as exprBuilder from './src/exprBuilder/index.js';
-export { Context, collection, core, decorators, exprBuilder };
-//# sourceMappingURL=index.js.map
--- src/exprBuilder/GroupExprBuilder.js
+++
@@ -1,8 +0,0 @@
-import BaseExprBuilder from './BaseExprBuilder.js';
-class GroupExprBuilder extends BaseExprBuilder {
-  expr(propName) {
-    return this._expr(propName);
-  }
-}
-export default GroupExprBuilder;
-//# sourceMappingURL=GroupExprBuilder.js.map
